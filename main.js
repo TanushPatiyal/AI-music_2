9a8364fc71f7ffd98bba8093c1e0ee36ba660795
TanushPatiyal <128584760+TanushPatiyal@users.noreply.github.com>
2023-04-04 13:48:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function setup(){
+    canvas = createCanvas(600, 500);
+    canvas.center();
+video = createCapture(VIDEO);
+video.hide();
+    }
+    function draw() {
+        image(video,0,0,600,500);
+    }
+    song = "";
+    song_2 ="";
+
+function preload(){
+song =loadSound("music2.mp3");
+song_2 =loadSound("music2.mp3"); }
+function play(){
+song.play() ;
+}
